86b5821917eb55e2f8d6168986eec211ad40c622
Gustav <gusan092@student.liu.se>
2019-11-07 08:32:14 +0100

    Viewmodel and command base

---
+++ TDDD49/Commands/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TDDD49.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action commandTask;
+
+        public RelayCommand(Action workToDo)
+        {
+            commandTask = workToDo;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            commandTask();
+        }
+    }
+}
